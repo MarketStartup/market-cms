d3bc5466f1ded755dc7065f1e1cef2e9f5c96f2f
venumarious <sayedisa96@gmail.com>
2025-11-22 19:24:01 +0530

    initial commit

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -7,7 +7,20 @@ export const Users: CollectionConfig = {
   },
   auth: true,
   fields: [
-    // Email added by default
-    // Add more fields as needed
+    { name: 'firstName', type: 'text' },
+    { name: 'lastName', type: 'text' },
+    {
+      name: 'role',
+      type: 'select',
+      required: true,
+      defaultValue: 'user',
+      options: [
+        { label: 'Admin', value: 'admin' },
+        { label: 'User', value: 'user' },
+      ],
+      admin: {
+        description: 'Only users with the "Admin" role can access the CMS.',
+      },
+    },
   ],
 }
