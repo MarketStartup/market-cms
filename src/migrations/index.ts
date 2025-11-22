229670c1501e56e12ef5a2fe7035fba39a767bf2
venumarious <sayedisa96@gmail.com>
2025-11-22 23:17:47 +0530

    migrations added

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20251122_174653 from './20251122_174653';
+
+export const migrations = [
+  {
+    up: migration_20251122_174653.up,
+    down: migration_20251122_174653.down,
+    name: '20251122_174653'
+  },
+];
